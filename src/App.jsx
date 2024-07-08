d7e9541a7150e1b9d3ada11f0e9fa017bff075b5
Ysla Martins <91572105+yslamartins@users.noreply.github.com>
2024-07-08 16:32:38 -0300

    Adicionei uma nova página que mostra um dashboard para selecionar uma opção de transporte para alugar.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Cadastro from './pages/Cadastro'
 import Login from './pages/Login'
 import Forgot from './pages/Forgot'
+import Dashboard from './pages/Dashboard'
+import Carrinho from './pages/Carrinho'
 
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
       <Route path='/' element={<Login/>} />
       <Route path='/cadastro' element={<Cadastro/>} />
       <Route path='/forgot' element={<Forgot/>} />
+      <Route path='/dashboard' element={<Dashboard/>} />
+      <Route path='/carros/:id' element={<Carrinho/>} />
     </Routes>
     </BrowserRouter>
   )
